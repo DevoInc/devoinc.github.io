e4754c643a429e756272f76b888a876f3496391c
Juan José Madrigal <juanjo.madrigal@devo.com>
2021-06-02 09:23:21 +0200

    docs

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Polymorphic Fixpoints: get rid of inheritance castings!",
+        "excerpt":"Castings in Java are considered a bad practice. Java’s strong static typing is a powerful compile-time safeguard. It checks that every method receives and returns objects with the features they are supposed to have. It makes sure that everything is well assembled. Castings open a backdoor in this mechanism by...","categories": ["Java"],
+        "tags": ["java","inheritance"],
+        "url": "/java/2021/04/23/polymorphic-fixpoints-get-rid-inheritance-castings.html",
+        "teaser": "/assets/images/posts/polymorphic_fixpoints/teaser.png"
+      },{
         "title": "Galidevo Galilei",
         "excerpt":"Galileo Galilei was born in Pisa in 1564 and lived a very adventurous life. He studied gravity, motion, inertia and astronomy among others. He was so important in his time that he even got invited to a barbecue that Pope Urban VIII was throwing in his honor. Ended up declining the...","categories": ["Troubleshooting"],
         "tags": ["javascript","python","troubleshooting","astronomy"],
